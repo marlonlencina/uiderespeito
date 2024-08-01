027fb9bfe94427a538def498460ab54de7e9b545
Marlon Lencina DevMarlon <marlonbmoraez@gmail.com>
2024-08-01 12:29:51 -0300

    First commit to repo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="selection:bg-violet-500" lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
